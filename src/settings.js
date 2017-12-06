f5f47e2ea04c0e476faad17535a2f7ef919214ef
kadai0308 <kadai0308@gmail.com>
2017-12-07 01:37:19 +0800

    edit backend url

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 const settings = {
-    // backend_url: 'http://140.114.75.146:8000'
-    backend_url: 'http://127.0.0.1:8000'
+    backend_url: 'http://140.114.75.146:8000'
+    // backend_url: 'http://127.0.0.1:8000'
 }
 
 export default settings
